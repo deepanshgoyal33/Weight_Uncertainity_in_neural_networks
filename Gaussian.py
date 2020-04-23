e773fa418dfb9366907b71e260391658f7ce4f78
Deepansh Goyal <deepanshgoyal33@gmail.com>
2020-04-23 22:31:48 +0530

    gaussian

--- Gaussian.py
+++ Gaussian.py
@@ -2,6 +2,9 @@ import torch
 
 
 class Gaussian(object):
+    """
+    Initialises the gaussian with mu and rho of the 
+    """
     def __init__(self,mu,rho):
         super().__init__()
         self.mu = mu
@@ -10,6 +13,10 @@ class Gaussian(object):
     
     #Properties
     def sigma(self):
+    """
+    Standarad deviation(sigma)= log(1+exp(rho))
+    """
+
         return torch.log1p(torch.exp(self.rho))
 
     def sample(self):
@@ -21,10 +28,20 @@ class Gaussian(object):
 
 #$ Scaled mixture of gaussians
 class ScaledMixtureGaussian(object):
+    """
+    Scaled mixture of the gaussians = p1(Gaussian1)+ (1-p)(Gaussian2)
+    """
     def __init__(self, sigma1, sigma2, p1 ):
         super().__init__()
         self.p1= p1
         self.sigma1= sigma1
         self.sigma2= sigma2
         self.gaussian1 = torch.distributions.Normal(0,sigma1)
-        self.
+        self.gaussian2 = torch.distributions.Normal(0,sigma2)
+
+    def scaledgaussian(self):
+        prob1 = torch.exp(self.gaussian1.log_prob(input))
+        prob2 = torch.exp(self.gaussian2.log_prob(input))
+        return (torch.log(self.p1 * prob1 + (1-self.p1) * prob2)).sum()
+
+    
